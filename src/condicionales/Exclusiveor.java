ea0a7a6dc6b660b9e905753040e289b8105cd3cd
albecabrera <albe.cabrera@yahoo.de>
2026-09-05 09:55:34 +0200

    Ejercicios exclusiv or

--- src/condicionales/Exclusiveor.java
+++ src/condicionales/Exclusiveor.java
@@ -1,4 +1,15 @@
 package condicionales;
 
 public class Exclusiveor {
+    public static void main(String[] args) {
+        // exclusive or
+        boolean jan_gewonnen = true;
+        boolean tim_gewonnen = true;
+
+        if (jan_gewonnen ^ tim_gewonnen) {
+            System.out.println("richtiges Ergebnis!");
+        } else {
+            System.out.println("Da stimmt was nicht...");
+        }
+    }
 }
--- src/condicionales/Uebung1.java
+++ src/condicionales/Uebung1.java
@@ -1,4 +1,17 @@
 package condicionales;
 
 public class Uebung1 {
+    public static void main(String[] args) {
+
+        double temperatur = 43;
+
+        if (temperatur < 20 || temperatur > 35) {
+            System.out.println("Unangehnem");
+        } else {
+            System.out.println("Angehnem");
+        }
+    }
 }
+
+
+
